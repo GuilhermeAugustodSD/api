bdc63dd890ee11217aac17b5785d5f0bf9d2bba4
Guilherme <guilhermednts2@gmail.com>
2022-08-08 18:26:23 -0300

    ajustes

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+const database = require("./database/sqlite");
+const AppError = require("./utils/AppError");
 const express = require('express');
 
 const routes = require("./routes");
@@ -6,8 +9,23 @@ const app = express();
 
 app.use(express.json());
 
+database();
+
 
 app.use(routes);
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+  console.error(error);
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
