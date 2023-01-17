984e70731a4a9f015615a14d1b88b865275b56ab
Guilherme Dantas <guilhermednts2@gmail.com>
2023-01-17 00:13:57 -0300

    atualizações

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const NotesController = require("../controllers/NotesController");
+
+
+const notesRoutes = Router();
+
+
+const notesController = new NotesController ()
+
+notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.get("/:id", notesController.show);
+
+module.exports = notesRoutes;
