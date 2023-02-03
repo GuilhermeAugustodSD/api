590858ee1641ef111425488fa54432b2239a0b2c
Guilherme <guilhermednts2@gmail.com>
2023-02-03 19:50:11 -0300

    Ajustes finais

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
+
+const cors = require("cors");
+
 
 const express = require('express');
 const routes = require("./routes");
@@ -8,7 +12,10 @@ const routes = require("./routes");
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 app.use((error, request, response, next) => {
